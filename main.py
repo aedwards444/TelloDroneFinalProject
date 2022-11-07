e4f555f390451b7cf9f8a211f457deec9884df07
adamB6 <asbotens@gmail.com>
2022-11-06 18:51:34 -0600

    Cam is opening in this code with openCV. also added some tutorial code that will probably come in handy in the future

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-#Hand Gesture Recognizer Tutorial!
-
 import cv2
 from djitellopy import Tello 
 import numpy as np
@@ -23,86 +21,85 @@ print(classNames)
 
 
 #Initialize the Drone
-#tello = Tello()
-#tello.connect()
-#tello.takeoff()
-
-
-# Initialize the webcam
-cap = cv2.VideoCapture(0)
-
+tello = Tello()
+tello.connect()
+tello.takeoff()
+tello.streamon()
 
 
 while True:
     # Read each frame from the webcam
-    _, frame = cap.read()
+    frame = tello.get_frame_read().frame
+    frame = cv2.resize(frame, (360, 240))
 
-    x, y, c = frame.shape
+    x = 360
+    y = 240
 
-    # Flip the frame vertically
-    frame = cv2.flip(frame, 1)
-    #
-    framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+#     # Flip the frame vertically
+#     frame = cv2.flip(frame, 1)
+#     #
+#     framergb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-    # Get hand landmark prediction
-    result = hands.process(framergb)
+#     # Get hand landmark prediction
+#     result = hands.process(framergb)
 
-    # print(result)
+#     # print(result)
     
-    className = ''
+#     className = ''
 
-    # post process the result
-    if result.multi_hand_landmarks:
-        landmarks = []
-        for handslms in result.multi_hand_landmarks:
-            for lm in handslms.landmark:
-                #print(id, lm)
-                lmx = int(lm.x * x)
-                lmy = int(lm.y * y)
+#     # post process the result
+#     if result.multi_hand_landmarks:
+#         landmarks = []
+#         for handslms in result.multi_hand_landmarks:
+#             for lm in handslms.landmark:
+#                 #print(id, lm)
+#                 lmx = int(lm.x * x)
+#                 lmy = int(lm.y * y)
 
-                landmarks.append([lmx, lmy])
+#                 landmarks.append([lmx, lmy])
 
-            # Drawing landmarks on frames
-            mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
+#             # Drawing landmarks on frames
+#             mpDraw.draw_landmarks(frame, handslms, mpHands.HAND_CONNECTIONS)
 
-            # Predict gesture
-            prediction = model.predict([landmarks])
-            # print(prediction)
-            classID = np.argmax(prediction)
-            className = classNames[classID]
+#             # Predict gesture
+#             prediction = model.predict([landmarks])
+#             # print(prediction)
+#             classID = np.argmax(prediction)
+#             className = classNames[classID]
 
-    # show the prediction on the frame
-    cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 
-                   1, (0,0,255), 2, cv2.LINE_AA)
-    if(className == "fist"):
-        print("This is a fist")
-        #tello.land()
-    elif(className == "peace"):
-        print("This is a peace sign")
-    elif(className == "okay"):
-        print("This is a okay sign")
-    elif(className == "thumbs up"):
-        print("This is a thumbs up")
-    elif(className == "thumbs down"):
-        print("This is a thumbs down")
-    elif(className == "call me"):
-        print("This is a call me sign")
-    elif(className == "stop"):
-        print("This is a stop sign")
-    elif(className == "rock"):
-        print("This is a rock sign")
-    elif(className == "live long"):
-        print("This is a okay sign")
-    elif(className == "smile"):
-        print("This is a smile sign")
+#     # show the prediction on the frame
+#     cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 
+#                    1, (0,0,255), 2, cv2.LINE_AA)
+#     if(className == "fist"):
+#         print("This is a fist")
+#         #tello.land()
+#     elif(className == "peace"):
+#         print("This is a peace sign")
+#     elif(className == "okay"):
+#         print("This is a okay sign")
+#     elif(className == "thumbs up"):
+#         print("This is a thumbs up")
+#     elif(className == "thumbs down"):
+#         print("This is a thumbs down")
+#     elif(className == "call me"):
+#         print("This is a call me sign")
+#     elif(className == "stop"):
+#         print("This is a stop sign")
+#     elif(className == "rock"):
+#         print("This is a rock sign")
+#     elif(className == "live long"):
+#         print("This is a okay sign")
+#     elif(className == "smile"):
+#         print("This is a smile sign")
    
     # Show the final output
     cv2.imshow("Output", frame) 
 
     if cv2.waitKey(1) == ord('q'):
+        tello.land()
         break
 
 # release the webcam and destroy all active windows
-cap.release()
+tello.streamoff()
 
 cv2.destroyAllWindows()
