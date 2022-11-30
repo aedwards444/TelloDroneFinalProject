d0e9ade804a8882a4d8f1a49d5f24b88bbb88786
adamB6 <asbotens@gmail.com>
2022-11-30 13:30:32 -0600

    seems to be working when not in flight

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 import cv2
-from djitellopy import Tello 
+import time
+from djitellopy import Tello
 import numpy as np
 import mediapipe as mp
 import tensorflow as tf
-from tensorflow.keras.models import load_model
+from keras.models import load_model
+from queue import Queue
 
+
+
+dataQueue = Queue(5)        
+        
 # Set frame height and width
-frameWidth = 960
-frameHeight = 720
+frameWidth = 600
+frameHeight = 400
 
 # initialize mediapipe
 mpHands = mp.solutions.hands
@@ -27,7 +33,6 @@ print(classNames)
 #Initialize the Drone
 tello = Tello()
 tello.connect()
-tello.takeoff()
 tello.streamoff()
 tello.streamon()
 
@@ -36,7 +41,10 @@ BAT_THRESH = 20
 
 print(tello.get_battery())
 
-while tello.get_battery() > BAT_THRESH:
+if tello.get_battery() > BAT_THRESH:
+    tello.takeoff()
+    tello.move_up(50)
+while True:
     # Read each frame from the webcam
     frame = tello.get_frame_read().frame
     frame = cv2.resize(frame, (frameWidth, frameHeight))
@@ -79,37 +87,49 @@ while tello.get_battery() > BAT_THRESH:
     # show the prediction on the frame
     cv2.putText(frame, className, (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 
                     1, (0,0,255), 2, cv2.LINE_AA)
-    if(className == "fist"):
-        print("This is a fist")
-        tello.land()
-    elif(className == "peace"):
-        print("This is a peace sign")
-    elif(className == "okay"):
-        print("This is a okay sign")
-    elif(className == "thumbs up"):
-        print("This is a thumbs up")
-    elif(className == "thumbs down"):
-        print("This is a thumbs down")
-    elif(className == "call me"):
-        print("This is a call me sign")
-    elif(className == "stop"):
-        print("This is a stop sign")
-    elif(className == "rock"):
-        print("This is a rock sign")
-    elif(className == "live long"):
-        print("This is a okay sign")
-    elif(className == "smile"):
-        print("This is a smile sign")
-   
-    # Show the final output
-    cv2.imshow("Output", frame) 
+
+    dataQueue.put(className)
     
-    if cv2.waitKey(1) == ord('q'):
-        tello.land()
-        tello.streamoff()
-        cv2.destroyAllWindows()
-        break
+    while(dataQueue.full()):
+        className = dataQueue.get()
+        if(className == "fist"):
+            print("This is a fist")
+            tello.land()
+        elif(className == "peace"):
+            print("This is a peace sign")
+            tello.flip_back()
+        elif(className == "okay"):
+            print("This is a okay sign")
+            tello.flip_forward()
+        elif(className == "thumbs down"):
+            print("This is a thumbs down")
+            tello.move_down(20)
+        elif(className == "thumbs up"):
+            print("This is a thumbs up sign")
+            if(tello.get_height() > 20):
+                tello.move_up(20)
+            else:
+                tello.takeoff()
+        elif(className == "stop"):
+            print("This is a stop sign")
+            tello.move_back(20)
+        elif(className == "rock"):
+            print("This is a rock sign")
+            tello.flip_left()
+            tello.flip_right()
+        elif(className == "live long"):
+            print("This is a okay sign")
+            tello.move_forward(20)
+        elif(className == "smile"):
+            print("This is a smile sign")
+    
+        # Show the final output
+        cv2.imshow("Output", frame) 
+        
+        if cv2.waitKey(1) == ord('q'):
+            break
 
 tello.land()
 tello.streamoff()
+tello.end()
 cv2.destroyAllWindows()
